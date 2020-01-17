eacc51d2f104772e85b7be1f83255eb81980999b
huagl <huagl@SY.COM>
2020-01-17 17:01:31 +0800

    pic.pl 20200117

--- pic.pl
+++ pic.pl
@@ -14,11 +14,12 @@ our $path = getcwd;
 our $ext;
 GetOptions ( 
     'cp=s'	        => \$compareGroup,
-    'h|help'	    => \$help,	
-    'p'             => \$ext,	    
+    'h|help'		=> \$help,	
+    'p'			=> \$ext,	    
 );
 my $usage =<<USAGE;
     -cp		    specify the comparegroup,something like RIF:Ctrl
+    -p		    process pictures only
     h|help	    help
 USAGE
 die "$usage" if $help;	
@@ -36,21 +37,21 @@ my %rename_hash = (
     'cv'			                    => 'Figure3.1',
     'CVbox'			                    => 'Figure3.2',
     'uniqP'			                    => 'Figure3.3',
-    'pepSeq.pepLength'		            => 'Figure3.4',
-    'coverage_pie'		                => 'Figure3.5',
-    'normprotRatiodistribution'	        => 'Figure4.1',
-    'Volcano'			                => 'Figure4.2',
-    'fun_stat'			                => 'Figure5.1',
-    'biological_process'	            => 'Figure5.2',
-    'cellular_component'	            => 'Figure5.2',
-    'molecular_function'	            => 'Figure5.2',
-    'go_compare_stat'		            => 'Figure5.3',
-    'All_ID.fa.cog'		                => 'Figure5.4',
-    'Pathway_compare_stat'	            => 'Figure5.6',
-    'CC-barchart'		                => 'Figure6.1',
-    'MF-barchart'		                => 'Figure6.1',
-    'BP-barchart'		                => 'Figure6.1',
-    '.path-bubble'		                => 'Figure6.3'    
+    'pepSeq.pepLength'				    => 'Figure3.4',
+    'coverage_pie'				    => 'Figure3.5',
+    'normprotRatiodistribution'			    => 'Figure4.1',
+    'Volcano'					    => 'Figure4.2',
+    'fun_stat'					    => 'Figure5.1',
+    'biological_process'			    => 'Figure5.2',
+    'cellular_component'			    => 'Figure5.2',
+    'molecular_function'			    => 'Figure5.2',
+    'go_compare_stat'				    => 'Figure5.3',
+    'All_ID.fa.cog'				    => 'Figure5.4',
+    'Pathway_compare_stat'			    => 'Figure5.6',
+    'CC-barchart'				    => 'Figure6.1',
+    'MF-barchart'				    => 'Figure6.1',
+    'BP-barchart'				    => 'Figure6.1',
+    '.path-bubble'				    => 'Figure6.3'    
 );
 my @old_name = keys %rename_hash;
 foreach(@old_name){
@@ -85,13 +86,12 @@ foreach(('GO','Pathway')){
     dircopy("$path\\Annotation\\$compareGroup2\_$_\_enrichment", "$path\\Project_data\\Enrichment\\$_\_enrichment\\$compareGroup2\_$_\_enrichment");
     copy("$path/function_stat/$compareGroup2\_$_\_compare_stat.pdf", "$path/Project_data/$_\_compare");
 }
-my $com;
 if($ext){exit;}
 my $workbook = Excel::Writer::XLSX -> new("$path/Project_data/Ident_Quant_result/Ident&Quant_SummaryResult.xlsx");
 my $worksheet = $workbook -> add_worksheet("Proteins");
 my $worksheet2 = $workbook -> add_worksheet("Spectra");
 my %font_head = (
-    font            => 'New Times Roman',
+    font            => 'Times New Roman',
     size            => 11,
     color           => 'white',
     bold            => 1,
@@ -147,6 +147,8 @@ close $PEPT;
 
 my $line2 = 1;
 my @sort = sort {$a <=> $b} keys %PEP;
+my $len = @sort;
+print_stat("writting Ident&Quant_SummaryResult.xlsx...");
 foreach(@sort){
     my $line = $_;
     foreach($PEP{$line}){
@@ -160,7 +162,6 @@ foreach(@sort){
         }
     }
 }
-print_stat("writting Ident&Quant_SummaryResult.xlsx...");
 sub print_stat{
     my ($mess) = @_;
     my $time = localtime;
