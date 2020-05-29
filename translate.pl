b31ff5abfb6e2d4ac8bd21501fa34112c210c0da
huangzhili4211 <52319982+huangzhili4211@users.noreply.github.com>
2020-05-29 11:33:44 +0800

    translate dna sequence to protein sequence
    translate dna sequence to protein sequence

--- translate.pl
+++ translate.pl
@@ -1,22 +1,10 @@
 #!/usr/bin/perl -w
+use strict;
 use Bio::SeqIO;
-my $file = $ARGV[0];
-my $in = Bio::SeqIO -> new(-file=>"$file",-format=>"fasta");
-my @dna;
-while(my $seq =$in->next_seq){
-    my $sequence = $seq -> seq;
-    push(@dna,$sequence);
-}
-open(FH,">pro.txt");
-foreach(@dna){
-    my $pep=&TranslateDNASeq($_);
-    print FH "$pep\n";
-}
-
-
-sub TranslateDNASeq{
-    use Bio::Seq;
-    (my $dna)=@_;
-    my $seqobj=Bio::Seq->new(-seq =>$dna, -alphabet =>'dna');
-    return $seqobj->translate()->seq();
+my $in = Bio::SeqIO->new(-file=>"10dna.fasta",-format=>"fasta");
+my $out = Bio::SeqIO->new(-file=>">result.fasta",-format=>"fasta");
+while(my $seq=$in->next_seq){
+    my $out_obj = $seq->translate;
+    $out->write_seq($out_obj);
+	
 }
